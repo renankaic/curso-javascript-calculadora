858164a165adda84429412289c20996d21cbed2f
Renan Kaic Lopes <renan-kaic@outlook.com>
2018-12-15 16:44:48 -0200

    Refactoring display date and time code. Adding an Interval to date and time.

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,7 +2,10 @@ class CalcController {
 
     constructor(){
 
-        this._displayCalc = "0";
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
 
@@ -10,29 +13,64 @@ class CalcController {
 
     initialize(){
 
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+        this.setDisplayDateTime();        
 
-        displayCalcEl.innerHTML = "4567";
-        dateEl.innerHTML = new Date().toLocaleDateString();
-        timeEl.innerHTML = new Date().toLocaleTimeString();
+        //The browser automatically generates a ID for a setInterval
+        let interval = setInterval(() => {
 
+            this.setDisplayDateTime();
+
+        }, 1000);
+
+        /* setTimeout(() => {
+
+            //Stops setInterval
+            clearInterval(interval);
+
+        }, 10000);*/
+
+    }
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }   
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }  
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
     }
 
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor){
-        this.displayCalc = valor;
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate(){
-        return this._currentDate;
+        return new Date();
     }
 
-    set currentDate(valor){
-        this._currentDate = valor;
+    set currentDate(value){
+        this._currentDate = value;
     }
 }
