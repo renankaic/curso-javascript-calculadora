8980d636ded5ccfe83074150a65e6ba57429c332
Renan Kaic Lopes <renan-kaic@outlook.com>
2018-12-15 17:30:07 -0200

    Adding button events using addEventListener

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
@@ -36,6 +37,15 @@ class CalcController {
         //Gets all element that matches with the concept
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
+        buttons.forEach( (btn, index) => {
+
+            btn.addEventListener('click', e => {
+
+                console.log(btn.className.baseVal.replace("btn-", ""));
+
+            });
+
+        });
     }
 
     setDisplayDateTime(){
