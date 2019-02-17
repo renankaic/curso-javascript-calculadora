4605d14b3f24f92bb291ed9a11d1602213dd1886
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-02-17 12:06:21 -0300

    Fix in number graphical overflow bug

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -476,6 +476,12 @@ class CalcController {
     }
 
     set displayCalc(value){
+
+        if ( value.toString().length > 10 ){
+            this.setError();
+            return false;
+        }
+
         this._displayCalcEl.innerHTML = value;
     }
 
