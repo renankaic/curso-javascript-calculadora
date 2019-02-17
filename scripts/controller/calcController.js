ac150d123bc01efd8a8f0e28555be98cf51d60a1
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-02-17 12:00:54 -0300

    Adding Audio on button click

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor(){
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -65,6 +67,33 @@ class CalcController {
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
 
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dblclick', e=>{
+
+                this.toggleAudio();
+
+            });
+
+        });
+
+    }
+
+    toggleAudio() {
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio() {
+
+        if ( this._audioOnOff ){
+
+            this._audio.currentTime = 0;
+            this._audio.play();
+
+        }
+
     }
 
     initKeyboard() {
@@ -74,6 +103,8 @@ class CalcController {
 
             //Logs the pressed key
             //console.log(e.key);
+
+            this.playAudio();
 
             switch (e.key){
 
@@ -326,6 +357,8 @@ class CalcController {
     }
 
     execBtn(value){
+
+        this.playAudio();
 
         switch (value){
 
